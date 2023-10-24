72de7603db0e45e54c26034b924432b917381849
SalvoBevilacqua <salvatorebevilacqua91@gmail.com>
2023-10-24 16:35:25 +0200

    ini

---
+++ js/script.js
@@ -0,0 +1,58 @@
+// array con le immagini da inserire
+const arrayImage = ["./img/01.jpg","./img/02.jpg","./img/03.jpg","./img/04.jpg","./img/05.jpg"];
+// test
+console.log(arrayImage[2]);
+
+// inizializzo ciò che andrà in html
+let stringImageForHtml = "";
+for (let i = 0; i < arrayImage.length; i++) {
+    stringImageForHtml += `<div class="item"><img src="${arrayImage[i]}" alt=""></div>`
+}
+
+console.log(stringImageForHtml);
+
+// selezione dove devo inserire la stringa
+const divSlider = document.querySelector(".items");
+
+// inserisco la stringa
+divSlider.innerHTML += stringImageForHtml;
+console.log(divSlider);
+
+// seleziono tutti i div con le immagini
+const slide = document.querySelectorAll(".item");
+console.log(slide);
+
+// inserisco la classe active nel primo elemento
+let index = 0;
+slide[index].classList.add("active");
+
+// inserisco la classe active nello slide
+document.querySelector(".next").addEventListener
+("click", function(event)
+{
+    event.preventDefault();
+    if(index < arrayImage.length - 1) {
+        slide[index].classList.remove("active");
+        index++;
+        slide[index].classList.add("active");
+    } else {
+        slide[index].classList.remove("active");
+        index = 0;
+        slide[index].classList.add("active");
+    }
+})
+
+document.querySelector(".prev").addEventListener
+("click", function(event)
+{
+    event.preventDefault();
+    if(index > 0){
+        slide[index].classList.remove("active");
+        index--;
+        slide[index].classList.add("active");
+    } else {
+        slide[index].classList.remove("active");
+        index = arrayImage.length -1;
+        slide[index].classList.add("active");        
+    }
+})
